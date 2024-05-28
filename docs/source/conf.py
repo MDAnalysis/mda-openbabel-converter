6acd8dafdd1b48eb1be04ca44aa9546d5ea496ea
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-05-29 06:51:12 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-
+# blahblah
 # In case the project was not installed
 import os
 import sys
